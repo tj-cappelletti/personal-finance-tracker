34a2490d6d79e4f24bbba5010bfd7854e0c71521
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2024-02-14 05:22:13 -0500

    Implemented fluent UI for main window
    Moved MainWindow to views namespace
    Created view model for MainWindow
    Added PageService for navigation

---
+++ src/PersonalFinanceTracker.WpfApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Wpf.Ui.Controls;
+
+namespace PersonalFinanceTracker.WpfApp.ViewModels;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    [ObservableProperty]private string _applicationTitle = string.Empty;
+
+    private bool _isInitialized;
+
+    [ObservableProperty]private ObservableCollection<object> _navigationFooter = [];
+
+    [ObservableProperty]private ObservableCollection<object> _navigationItems = [];
+
+    [ObservableProperty]private ObservableCollection<MenuItem> _trayMenuItems = [];
+
+    public MainWindowViewModel()
+    {
+        if (!_isInitialized)
+            InitializeViewModel();
+    }
+
+    private void InitializeViewModel()
+    {
+        ApplicationTitle = "WPF UI - MVVM Demo";
+
+        NavigationItems = [];
+
+        NavigationFooter = [];
+
+        TrayMenuItems = [new MenuItem { Header = "Home", Tag = "tray_home" }];
+
+        _isInitialized = true;
+    }
+}
---
+++ src/PersonalFinanceTracker.WpfApp/Views/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows;
+using PersonalFinanceTracker.WpfApp.ViewModels;
+using Wpf.Ui;
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
+
+namespace PersonalFinanceTracker.WpfApp.Views;
+
+public partial class MainWindow : INavigationWindow
+{
+    public MainWindowViewModel ViewModel { get; }
+
+    public MainWindow(
+        MainWindowViewModel viewModel,
+        IPageService pageService,
+        INavigationService navigationService)
+    {
+        DataContext = this;
+
+        ViewModel = viewModel;
+
+        SystemThemeWatcher.Watch(this);
+
+        InitializeComponent();
+
+        SetPageService(pageService);
+
+        navigationService.SetNavigationControl(NavigationView);
+    }
+
+    public void CloseWindow()
+    {
+        Close();
+    }
+
+    public INavigationView GetNavigation()
+    {
+        return NavigationView;
+    }
+
+    public bool Navigate(Type pageType)
+    {
+        return NavigationView.Navigate(pageType);
+    }
+
+    protected override void OnClosed(EventArgs e)
+    {
+        base.OnClosed(e);
+
+        Application.Current.Shutdown();
+    }
+
+    public void SetPageService(IPageService pageService)
+    {
+        NavigationView.SetPageService(pageService);
+    }
+
+    public void SetServiceProvider(IServiceProvider serviceProvider)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ShowWindow()
+    {
+        Show();
+    }
+}
